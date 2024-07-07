df975eacfb4e1df73c42ba090af61f5956aaa420
Movsar Makhmutov <m.movsar@gmail.com>
2024-07-07 21:51:00 +0300

    rrd

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { store } from './store/store.ts'
 import App from './App.tsx'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
