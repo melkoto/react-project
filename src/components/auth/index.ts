4bf3757e9933396779fd0294740db933b50e789b
Movsar Makhmutov <m.movsar@gmail.com>
2024-07-29 18:22:53 +0300

    init

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -3,4 +3,5 @@ import { lazy } from 'react'
 export const LazyLogin = lazy(() => import('./login-component'))
 export const LazyRegister = lazy(() => import('./register-component'))
 export const LazyAuthPage = lazy(() => import('./auth-page-component'))
+
 export { default as ProtectedRoute } from './protected-route'
